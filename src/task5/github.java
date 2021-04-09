6c311f372ec1cddd8a23085f59eceb50605fb256
Vicky015 <vickyvettel15@gmail.com>
2021-04-09 13:10:16 +0530

    task5

--- src/task5/github.java
+++ src/task5/github.java
@@ -11,5 +11,16 @@ public class github {
      System.out.println(3);
 		}
 	
+		private void method4() {
+			System.out.println(4);
+		}
+
+		private void method5() {
+			System.out.println(5);
+		}
+
+		private void method6() {
+			System.out.println(6);
+		}
 
 }
